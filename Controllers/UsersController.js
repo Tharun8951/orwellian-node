818a5bc5fe894082b532b3a4c1684c062576942a
tharunbytes <tharunappu2004@gmail.com>
2023-09-23 12:45:21 +0530

    changed user controllers

--- Controllers/UsersController.js
+++ Controllers/UsersController.js
@@ -1,47 +1,76 @@
+const UsersModels = require('../Models/Users.models')
 const UserModel = require('../Models/Users.models')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 const createToken = (_id) => {
-    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '7d'})
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '7d' })
 }
 
 const register = async (req, res) => {
-    const {username, email, password} = req.body
+  //   const { username, email, password } = req.body
 
-    try {
-        const user = await UserModel.signup(username, email, password)
+  try {
+    // const user = await UserModel.signup(username, email, password)
+    const { username, email, password } = req.body
+    const saltRounds = 10
+    const hashedPassword = await bcrypt.hash(password, saltRounds)
 
-        //create a token
-        const token = createToken(user._id)
-        res.status(200).json({
-            token: token
-        })
-    } catch (error) {
-        console.log(error)
-        res.status(400).json({
-            msg: error.message
-        })
+    const exists = await UsersModels.findOne({ username })
+    if (exists) {
+      return res.json({
+        msg: 'Username already exists',
+      })
     }
+    const user = await UsersModels.create({
+      username,
+      email,
+      password: hashedPassword,
+    })
+    // res.json({ success: true })
+
+    //create a token
+    const token = createToken(user._id)
+    res.status(200).json({
+      token: token,
+      user: user,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({
+      msg: error.message,
+    })
+  }
 }
 
 const login = async (req, res) => {
-    const {username, password} = req.body
-
-    try {
-        const user = await UserModel.login(username, password)
-
-        //create a token
-        const token = createToken(user._id)
-        res.status(200).json({
-            token: token
+  try {
+    const { username, password } = req.body
+    // const user = await UserModel.login(username, password)
+    const userdata = await UsersModels.findOne({ username })
+    if (userdata) {
+      const passwordMatch = await bcrypt.compare(password, userdata.password)
+      if (passwordMatch) {
+        const token = createToken(userdata._id)
+        return res.status(200).json({
+          token: token
         })
-    } catch (error) {
-        console.log(error)
-        res.status(400).json({
-            msg: error.message
+      } else {
+        return res.json({
+            msg: 'Incorrect password'
+        })
+      }
+    } else {
+        return res.json({
+            msg: 'Please signup first'
         })
     }
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({
+      msg: error.message,
+    })
+  }
 }
 
-
-module.exports = { register, login}
+module.exports = { register, login }
--- Models/Users.models.js
+++ Models/Users.models.js
@@ -20,48 +20,48 @@ const userSchema = new mongoose.Schema({
 })
 
 // static signup method
-userSchema.statics.signup = async function(username, email, password) {
+// userSchema.statics.signup = async function(username, email, password) {
 
-  //validation
-  if(!username || !email || !password){
-    throw Error('All fields must be filled')
-  }
+//   //validation
+//   if(!username || !email || !password){
+//     throw Error('All fields must be filled')
+//   }
 
-  if(!validator.isEmail(email)){
-    throw Error('Email is not valid')
-  }
+//   if(!validator.isEmail(email)){
+//     throw Error('Email is not valid')
+//   }
 
-  // if(!validator.isStrongPassword(password)){
-  //   throw Error('Use a strong password')
-  // }
+//   // if(!validator.isStrongPassword(password)){
+//   //   throw Error('Use a strong password')
+//   // }
 
-  const exists = await this.findOne({ username }).maxTime(20000)
-  if (exists) {
-    throw Error('Username already in use')
-  }
+//   const exists = await this.findOne({ username })
+//   if (exists) {
+//     throw Error('Username already in use')
+//   }
 
-  const salt = await bcrypt.genSalt(10)
-  const hash = await bcrypt.hash(password, salt)
+//   const salt = await bcrypt.genSalt(10)
+//   const hash = await bcrypt.hash(password, salt)
 
-  const user = await this.create({ username, email, password: hash }).maxTime(20000)
-  return user
-}
+//   const user = await this.create({ username, email, password: hash })
+//   return user
+// }
 
 //static login method
-userSchema.statics.login = async function(username, password){
-  if(!username || !password){
-    throw Error('All fields must be filled')
-  }
-  const user = await this.findOne({ username }).maxTime(20000)
-  if (!user) {
-    throw Error('please signup')
-  }
-  const match = await bcrypt.compare(password, user.password)
-  if(!match){
-    throw Error('Incorrect password')
-  }
+// userSchema.statics.login = async function(username, password){
+//   if(!username || !password){
+//     throw Error('All fields must be filled')
+//   }
+//   const user = await this.findOne({ username })
+//   if (!user) {
+//     throw Error('please signup')
+//   }
+//   const match = await bcrypt.compare(password, user.password)
+//   if(!match){
+//     throw Error('Incorrect password')
+//   }
 
-  return user
-}
+//   return user
+// }
 
 module.exports = mongoose.model('User', userSchema)
